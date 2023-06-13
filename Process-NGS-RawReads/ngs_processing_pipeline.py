003823042c19403838088ad9240eaf720eeee3d1
earezza <39600627+earezza@users.noreply.github.com>
2023-06-13 09:05:57 -0400

    Update ngs_processing_pipeline.py

--- Process-NGS-RawReads/ngs_processing_pipeline.py
+++ Process-NGS-RawReads/ngs_processing_pipeline.py
@@ -477,7 +477,7 @@ def AdapterTrim_Cutadapt():
             # Trim poly-A
             if args.technique == 'rnaseq' and args.polyAtrim:
                 try:
-                    result = subprocess.run(('cutadapt --cores=0 %s -o %sAll_output/Trimmed_reads/%s_Trimmed_R1.fastq -p %sAll_output/Trimmed_reads/%s_Trimmed_R2.fastq %sAll_output/Trimmed_reads/%s_Trimmed_R1.fastq.gz %sAll_output/Trimmed_reads/%s_Trimmed_R2.fastq.gz'%('-poly-a', OUT_DIR, f, OUT_DIR, f, OUT_DIR, f, OUT_DIR, f)), shell=True, capture_output=True, text=True)
+                    result = subprocess.run(('cutadapt --cores=0 %s -o %sAll_output/Trimmed_reads/%s_Trimmed_R1.fastq -p %sAll_output/Trimmed_reads/%s_Trimmed_R2.fastq %sAll_output/Trimmed_reads/%s_Trimmed_R1.fastq %sAll_output/Trimmed_reads/%s_Trimmed_R2.fastq'%('--poly-a', OUT_DIR, f, OUT_DIR, f, OUT_DIR, f, OUT_DIR, f)), shell=True, capture_output=True, text=True)
                     logger.info(result.stdout.rstrip('\n'))
                     logger.warning(result.stderr.rstrip('\n'))
                 except Exception as e:
@@ -502,7 +502,7 @@ def AdapterTrim_Cutadapt():
             # Trim poly-A
             if args.technique == 'rnaseq' and args.polyAtrim:
                 try:
-                    result = subprocess.run(('cutadapt --cores=0 %s %s -o %sAll_output/Trimmed_reads/%s_Trimmed.fastq %sAll_output/Trimmed_reads/%s_Trimmed.fastq.gz'%(TRIM, '-poly-a', OUT_DIR, f, OUT_DIR, f)), shell=True, capture_output=True, text=True)
+                    result = subprocess.run(('cutadapt --cores=0 %s %s -o %sAll_output/Trimmed_reads/%s_Trimmed.fastq %sAll_output/Trimmed_reads/%s_Trimmed.fastq'%(TRIM, '--poly-a', OUT_DIR, f, OUT_DIR, f)), shell=True, capture_output=True, text=True)
                     logger.info(result.stdout.rstrip('\n'))
                     logger.warning(result.stderr.rstrip('\n'))
                 except Exception as e:
