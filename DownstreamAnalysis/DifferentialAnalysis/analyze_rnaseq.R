53a0e25b5c944dbcb3afd5ccbb51fee7999d3a4f
earezza <39600627+earezza@users.noreply.github.com>
2024-10-09 14:45:21 -0400

    Update analyze_rnaseq.R

--- DownstreamAnalysis/DifferentialAnalysis/analyze_rnaseq.R
+++ DownstreamAnalysis/DifferentialAnalysis/analyze_rnaseq.R
@@ -146,6 +146,7 @@ make_dotplot <- function(df, title="", ylabel="Description", colour="#56B1F7", n
   for (d in 1:length(df$Description)){
     i <- 1
     s <- df$Description[d]
+    s <- str_remove(s, " - Mus musculus \\(house mouse\\)")
     df$Description[d] <- ""
     while (i < length(strsplit(s, ' ')[[1]]) + 1){
       df$Description[d] <- paste(df$Description[d], paste(strsplit(s, ' ')[[1]][i:(i+5)], collapse = ' '), sep='\n')
